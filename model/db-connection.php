ff29e0ef41ff181367cd70c76f8db497a7be30b8
rauldmj3 <r.mingo@sapalomera.cat>
2023-10-23 18:08:06 +0200

    Exercici 8

--- model/db-connection.php
+++ model/db-connection.php
@@ -1,6 +1,6 @@
-
-
 <?php
+
+include "env.php";
 
 /**
  * Crea una nova connexió amb la base de dades
@@ -10,7 +10,8 @@
 function getConnection()
 {
     try {
-        return new PDO(sprintf('mysql:host=%s;dbname=%s',"localhost", "examuf1_db"), "root","");
+        //return new PDO(sprintf('mysql:host=%s;dbname=%s',"localhost", "examuf1_db"), "root",""); EXERCICI 8
+        return new PDO(sprintf('mysql:host=%s;dbname=%s',host, database), user,password);
     } catch (PDOException $e) {
         die("No es pot establir connexió amb la base de dades");
     }
---
+++ model/env.php
@@ -0,0 +1,8 @@
+<?php
+//Exercici 8
+define("database","examuf1_db");
+define("user","root");
+define("password","");
+define("host","localhost");
+
+?>
